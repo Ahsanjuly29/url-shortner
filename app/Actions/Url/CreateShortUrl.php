363f88e8ebe4cf86a84096afe78183d3aaa22ec8
ahsan.july29 <ahsan.july29@gmail.com>
2024-09-29 21:55:07 +0600

    update

--- app/Actions/Url/CreateShortUrl.php
+++ app/Actions/Url/CreateShortUrl.php
@@ -13,15 +13,16 @@ class CreateShortUrl
         $shortCode = Str::random(6); // Creating randonm String(6 char)
         $userId = (Auth::check() == false) ? 1 : Auth::user()->id; //1 is guest id
 
-        // Stop shortening Duplicate Url 
+        // Stop shortening Duplicate Url
         return Url::updateOrCreate(
             [
                 'long_url' => $longUrl,
+                'user_id' => $userId,
             ],
             [
                 'long_url' => $longUrl,
-                'short_code' => $shortCode,
                 'user_id' => $userId,
+                'short_code' => $shortCode,
             ]);
     }
 }
