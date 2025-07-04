b4d96f04cebb6040d7d7e116e14dd9fa386509e6
ahsanjuly29 <ahsan.july29@gmail.com>
2025-07-05 03:33:26 +0600

    up

--- resources/views/url/dashboard.blade.php
+++ resources/views/url/dashboard.blade.php
@@ -51,7 +51,7 @@
                             </td>
                         </tr>
                     @endforeach
-                    @if ($eachItem->count() > 0)
+                    @if ($eachItem?->count() > 0)
                         <tr>
                             <td colspan="13">
                                 <button id="multiple_delete_btn" class="btn btn-xs btn-danger mr-2 d-none" type="submit">
